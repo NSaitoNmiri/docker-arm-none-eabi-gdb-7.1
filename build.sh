b21a8397ea118476f006ae9dac28406a24d1d6a7
SAITO Naoki <saito.naoki@nmiri.city.nagoya.jp>
2017-05-08 17:25:37 +0900

    modify build script to add a gdb binary's suffix.

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
 set -eu
-wget -q http://ftp.gnu.org/gnu/gdb/gdb-7.1a.tar.bz2
-tar xvf gdb-7.1a.tar.bz2
-patch -p0 < gdb-71.diff
+if [ ! -d gdb-7.1 ]; then
+  if [ ! -f gdb-7.1a.tar.bz2 ]; then
+    curl http://ftp.gnu.org/gnu/gdb/gdb-7.1a.tar.bz2 -o gdb-7.1a.tar.bz2
+  fi
+  tar xvf gdb-7.1a.tar.bz2
+fi
+patch -p0 < ${SCRIPT_DIR}/gdb-71.diff
 mkdir -p gdb-7.1/build
 cd gdb-7.1/build
-../configure --target=arm-none-eabi --prefix=/usr/local --enable-interwork --enable-multilib
+../configure --target=arm-none-eabi --prefix=/usr/local --program-suffix=-7.1 --enable-interwork --enable-multilib
 make all
 make install
 
